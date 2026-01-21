1d1f0ab62fddbd42de310995ad8e9942f65b5755
Lillian McDill <lmcdill@stu.jsu.edu>
2026-01-20 19:39:40 -0500

    Added f3() methods

--- Class1.java
+++ Class1.java
@@ -11,5 +11,8 @@ public class Class1 {
     public void f2() {
     System.out.println("Feature f2");
     }
+    public void f3() {
+    System.out.println("Feature f3");
+    }
     
 }
--- HelloWorld.java
+++ HelloWorld.java
@@ -17,5 +17,8 @@ public class HelloWorld {
     public void f2() {
     System.out.println("Feature f2");
     }
+    public void f3() {
+    System.out.println("Feature f3");
+    }
 
 }
